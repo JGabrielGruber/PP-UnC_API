288007e31470455ffefd56a2784b31145d4a5f5b
José Gabriel Gruber <jgabrielgruber@hotmail.com>
2019-08-28 19:29:09 -0300

    Criada estrutura base do projeto; Criado App do Hug; Criado Configs;

---
+++ src/conf.py
@@ -0,0 +1,18 @@
+import	mongoengine
+
+def getConf():
+	if not os.path.exists('./.cache'):
+		os.makedirs('./.cache')
+	if os.path.exists('./.cache/conf.json'):
+		with open('./.cache/conf.json', mode='rb') as privatefile:
+			return	json.loads(privatefile.read())['confs']
+	else:
+		with open('./.cache/conf.json', mode='w') as file:
+			file.write(json.dumps( { "confs" : {
+				"db_auth_host": "",
+				"db_data_host": ""
+			} } ))
+		getSecret()
+
+db_auth	= mongoengine.connect(host=getConf()['db_auth_host'], alias="auth").PPUnC_Auth
+db_data	= mongoengine.connect(host=getConf()['db_data_host'], alias="default").PPUnC_Data
