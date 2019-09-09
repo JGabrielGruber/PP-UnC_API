ca4dd473bfbf81ebee19ce048df55c848f229244
José Gabriel Gruber <jgabrielgruber@hotmail.com>
2019-09-09 18:22:41 -0300

    Corrigidos erros de sintaxe; Corrigida exclusão de turma; Implementado tratamento dos dados de retorno da turma;

--- src/controller/turma.py
+++ src/controller/turma.py
@@ -8,10 +8,9 @@ from	model.turma		import TurmaType, Turma
 from	controller		import auth
 
 def getTurmas(response, usuario_id, materia_id):
-	locals	= eval(response.get_header("locals"))
 	try:
 		turmas	= Usuario.objects.get(id=usuario_id).materias.get(_id=materia_id).turmas
-		data		= []
+		data	= []
 		if turmas:
 			for turma in turmas:
 				data.append(json.loads(turma.to_json()))
@@ -24,19 +23,17 @@ def getTurmas(response, usuario_id, materia_id):
 		return { "error": "bad_gateway" }
 
 def getTurmaById(response, usuario_id, materia_id, turma_id):
-	locals	= eval(response.get_header("locals"))
 	try:
 		turma		= Usuario.objects.get(id=usuario_id).materias.get(_id=materia_id).turmas.get(_id=turma_id)
 		data		= []
 		if turma:
 			data	= json.loads(turma.to_json())
-		return data
+		return dataTurma(data)
 	except Exception as e:
 		response.status = HTTP_502
 		return { "error": "bad_gateway" }
 
 def newTurma(response, usuario_id, materia_id, data):
-	locals	= eval(response.get_header("locals"))
 	try:
 		data.pop("alunos", None)
 		data.pop("provas", None)
@@ -45,34 +42,48 @@ def newTurma(response, usuario_id, materia_id, data):
 		usuario.materias.get(_id=materia_id).turmas.append(turma)
 		usuario.save()
 		response.status = HTTP_201
-		return json.loads(turma.to_json())
+		return dataTurma(json.loads(turma.to_json()))
 	except Exception as e:
 		response.status = HTTP_502
 		return { "error": "bad_gateway" }
 
 def updateTurma(response, usuario_id, materia_id, turma_id, data):
-	locals	= eval(response.get_header("locals"))
 	try:
 		usuario	= Usuario.objects.get(id=usuario_id)
 		turma	= usuario.materias.get(_id=materia_id).turmas.get(_id=turma_id)
 		data.pop("timestamp", None)
 		data.pop("alunos", None)
 		data.pop("provas", None)
+		data.pop("_id", None)
 		data["timeupdate"]	= datetime.now()
 		for key, value in data.items():
 			turma[key]	= value
 		usuario.save()
-		return json.loads(turma.to_json())
+		return dataTurma(json.loads(turma.to_json()))
 	except Exception as e:
 		response.status = HTTP_502
 		return { "error": "bad_gateway" }
 
 def deleteTurma(response, usuario_id, materia_id, turma_id):
-	locals	= eval(response.get_header("locals"))
 	try:
-		turma	= Usuario.objects.get(id=usuario_id).materias.get(_id=materia_id).turmas.get(_id=turma_id)
-		turma.delete()
-		return {}
+		usuario	= Usuario.objects.get(id=usuario_id)
+		usuario.materias.get(_id=materia_id).turmas.filter(_id=turma_id).delete()
+		usuario.save()
+		return
 	except Exception as e:
 		response.status = HTTP_502
 		return { "error": "bad_gateway" }
+
+def dataTurma(data):
+	for item in data["alunos"]:
+		item.pop('email', None)
+		item.pop('timestamp', None)
+		item.pop('timeupdate', None)
+	for item in data["provas"]:
+		item.pop('descricao', None)
+		item.pop('duracao', None)
+		item.pop('questoes', None)
+		item.pop('realizacoes', None)
+		item.pop('timestamp', None)
+		item.pop('timeupdate', None)
+	return data
