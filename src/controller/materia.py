06fd4257aab447702a818f460457b83a245fd9ba
José Gabriel Gruber <jgabrielgruber@hotmail.com>
2019-09-09 10:39:32 -0300

    Corrigidos erros de sintaxe; Implementado tratamento dos dados retornados pela Matéria; COrrigida Atualização; COrrigida Remoção;

--- src/controller/materia.py
+++ src/controller/materia.py
@@ -7,7 +7,6 @@ from	model.materia	import MateriaType, Materia
 from	controller		import auth
 
 def getMaterias(response, usuario_id):
-	locals	= eval(response.get_header("locals"))
 	try:
 		materias	= Usuario.objects.get(id=usuario_id).materias
 		data		= []
@@ -19,17 +18,15 @@ def getMaterias(response, usuario_id):
 			item.pop('provasBases', None)
 		return data
 	except Exception as e:
-		print(e)
 		response.status = HTTP_502
 		return { "error": "bad_gateway" }
 
 def getMateriaById(response, usuario_id, materia_id):
-	locals	= eval(response.get_header("locals"))
 	try:
 		materia		= Usuario.objects.get(id=usuario_id).materias.get(_id=materia_id)
 		data		= []
 		if materia:
-			data	= json.loads(materia.to_json())
+			data	= dataMateria(json.loads(materia.to_json()))
 		return data
 	except Exception as e:
 		response.status = HTTP_502
@@ -45,7 +42,7 @@ def newMateria(response, usuario_id, data):
 		usuario.materias.append(materia)
 		usuario.save()
 		response.status = HTTP_201
-		return json.loads(materia.to_json())
+		return dataMateria(json.loads(materia.to_json()))
 	except Exception as e:
 		response.status = HTTP_502
 		return { "error": "bad_gateway" }
@@ -57,22 +54,36 @@ def updateMateria(response, usuario_id, materia_id, data):
 		materia	= usuario.materias.get(_id=materia_id)
 		data.pop("timestamp", None)
 		data.pop("turmas", None)
-		data.pop("provasBases", None)
+		data.pop("provas_bases", None)
 		data["timeupdate"]	= datetime.now()
 		for key, value in data.items():
 			materia[key]	= value
 		usuario.save()
-		return json.loads(materia.to_json())
+		return dataMateria(json.loads(materia.to_json()))
 	except Exception as e:
 		response.status = HTTP_502
 		return { "error": "bad_gateway" }
 
-def deleteMateria(response, usuario_id, materia_id):
+def deleteMateriaById(response, usuario_id, materia_id):
 	locals	= eval(response.get_header("locals"))
 	try:
-		materia	= Usuario.objects.get(id=usuario_id).materias.get(_id=materia_id)
-		materia.delete()
-		return {}
+		Usuario.objects(id=usuario_id).update_one(pull__materias___id=materia_id)
+		return
 	except Exception as e:
 		response.status = HTTP_502
 		return { "error": "bad_gateway" }
+
+def dataMateria(data):
+	for item in data["turmas"]:
+		item.pop('alunos', None)
+		item.pop('provas', None)
+		item.pop('semestre', None)
+		item.pop('descricao', None)
+		item.pop('timestamp', None)
+		item.pop('timeupdate', None)
+	for item in data["provas_bases"]:
+		item.pop('questoes', None)
+		item.pop('descricao', None)
+		item.pop('timestamp', None)
+		item.pop('timeupdate', None)
+	return data
