e08f27c373b271e70542d3ad7a4cf22f8ffd9ee3
José Gabriel Gruber <jgabrielgruber@hotmail.com>
2019-09-09 11:24:43 -0300

    Corrigidos erros de sintaxe; Corrigida remoção de Prova Base;

--- src/controller/provaBase.py
+++ src/controller/provaBase.py
@@ -3,25 +3,24 @@ from	falcon		import HTTP_201, HTTP_400, HTTP_403, HTTP_502
 from	datetime	import datetime
 
 from	model.usuario	import Usuario
-from	model.materia	import Materia
 from	model.provaBase	import ProvaBaseType, ProvaBase
 from	controller		import auth
 
 def getProvaBases(response, usuario_id, materia_id):
-	locals	= eval(response.get_header("locals"))
 	try:
 		provasBases	= Usuario.objects.get(id=usuario_id).materias.get(_id=materia_id).provas_bases
 		data		= []
 		if provasBases:
 			for provaBase in provasBases:
 				data.append(json.loads(provaBase.to_json()))
+		for item in data:
+			item.pop('questoes', None)
 		return data
 	except Exception as e:
 		response.status = HTTP_502
 		return { "error": "bad_gateway" }
 
 def getProvaBaseById(response, usuario_id, materia_id, provaBase_id):
-	locals	= eval(response.get_header("locals"))
 	try:
 		provaBase	= Usuario.objects.get(id=usuario_id).materias.get(_id=materia_id).provas_bases.get(_id=provaBase_id)
 		data		= []
@@ -33,7 +32,6 @@ def getProvaBaseById(response, usuario_id, materia_id, provaBase_id):
 		return { "error": "bad_gateway" }
 
 def newProvaBase(response, usuario_id, materia_id, data):
-	locals	= eval(response.get_header("locals"))
 	try:
 		usuario		= Usuario.objects.get(id=usuario_id)
 		provaBase	= ProvaBase(**data)
@@ -42,12 +40,10 @@ def newProvaBase(response, usuario_id, materia_id, data):
 		response.status = HTTP_201
 		return json.loads(provaBase.to_json())
 	except Exception as e:
-		print(e)
 		response.status = HTTP_502
 		return { "error": "bad_gateway" }
 
 def updateProvaBase(response, usuario_id, materia_id, provaBase_id, data):
-	locals	= eval(response.get_header("locals"))
 	try:
 		usuario		= Usuario.objects.get(id=usuario_id)
 		provaBase	= usuario.materias.get(_id=materia_id).provas_bases.get(_id=provaBase_id)
@@ -61,12 +57,12 @@ def updateProvaBase(response, usuario_id, materia_id, provaBase_id, data):
 		response.status = HTTP_502
 		return { "error": "bad_gateway" }
 
-def deleteProvaBase(response, usuario_id, materia_id, provaBase_id):
-	locals	= eval(response.get_header("locals"))
+def deleteProvaBaseById(response, usuario_id, materia_id, provaBase_id):
 	try:
-		provaBase	= Usuario.objects.get(id=usuario_id).materias.get(_id=materia_id).provas_bases.get(_id=provaBase_id)
-		provaBase.delete()
-		return {}
+		usuario	= Usuario.objects.get(id=usuario_id)
+		usuario.materias.get(_id=materia_id).provas_bases.filter(_id=provaBase_id).delete()
+		usuario.save()
+		return
 	except Exception as e:
 		response.status = HTTP_502
 		return { "error": "bad_gateway" }
