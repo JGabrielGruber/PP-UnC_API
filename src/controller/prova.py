d260095f0c33a65f44017d3cad3f03865af1f430
José Gabriel Gruber <jgabrielgruber@hotmail.com>
2019-09-09 18:43:26 -0300

    Corrigidos erros de sintaxe; Corrigida atuaçização de prova; Implementado adição, remoção e atualização de questões; Corrigida remoção de prova;

--- src/controller/prova.py
+++ src/controller/prova.py
@@ -4,9 +4,9 @@ from	datetime	import datetime
 
 from	model.usuario	import Usuario
 from	model.prova		import ProvaType, Prova
+from	model.questao	import Questao
 
 def getProvas(response, usuario_id, materia_id, turma_id):
-	locals	= eval(response.get_header("locals"))
 	try:
 		provas	= Usuario.objects.get(id=usuario_id).materias.get(_id=materia_id).turmas.get(_id=turma_id).provas
 		data	= []
@@ -22,39 +22,67 @@ def getProvas(response, usuario_id, materia_id, turma_id):
 		return { "error": "bad_gateway" }
 
 def getProvaById(response, usuario_id, materia_id, turma_id, prova_id):
-	locals	= eval(response.get_header("locals"))
 	try:
 		prova	= Usuario.objects.get(id=usuario_id).materias.get(_id=materia_id).turmas.get(_id=turma_id).provas.get(_id=prova_id)
 		data	= []
 		if prova:
-			data	= json.loads(prova.to_json())
+			data	= dataProva(json.loads(prova.to_json()))
 		return data
 	except Exception as e:
 		response.status = HTTP_502
 		return { "error": "bad_gateway" }
 
 def newProva(response, usuario_id, materia_id, turma_id, data):
-	locals	= eval(response.get_header("locals"))
 	try:
 		data.pop("realizacoes", None)
+		data.pop("questoes", None)
 		usuario	= Usuario.objects.get(id=usuario_id)
 		prova	= Prova(**data)
 		usuario.materias.get(_id=materia_id).turmas.get(_id=turma_id).provas.append(prova)
 		usuario.save()
 		response.status = HTTP_201
-		return json.loads(prova.to_json())
+		return dataProva(json.loads(prova.to_json()))
 	except Exception as e:
 		response.status = HTTP_502
 		return { "error": "bad_gateway" }
 
 def updateProva(response, usuario_id, materia_id, turma_id, prova_id, data):
-	locals	= eval(response.get_header("locals"))
 	try:
 		usuario	= Usuario.objects.get(id=usuario_id)
 		prova	= usuario.materias.get(_id=materia_id).turmas.get(_id=turma_id).provas.get(_id=prova_id)
+		ids			= []
+
 		data.pop("timestamp", None)
 		data.pop("realizacoes", None)
+		data.pop("_id", None)
 		data["timeupdate"]	= datetime.now()
+		if data["questoes"]:
+			for key, value in enumerate(data["questoes"]):
+				if not "_id" in value:
+					questao	= Questao(**value)
+					prova.questoes.append(questao)
+					ids.append(questao["_id"])
+					value	= json.loads(questao.to_json())
+				else:
+					questao	= prova.questoes.get(_id=value["_id"])
+					ids.append(questao["_id"])
+					for k, v in value.items():
+						questao[k]	= v
+			data.pop("questoes")
+			usuario.save()
+			usuario.reload()
+			prova		= usuario.materias.get(_id=materia_id).turmas.get(_id=turma_id).provas.get(_id=prova_id)
+			questoes	= prova.questoes
+			for questao in questoes:
+				if questao["_id"] in ids:
+					ids.pop(ids.index(questao["_id"]))
+				else:
+					ids.append(questao["_id"])
+			for id in ids:
+				prova.questoes.filter(_id=id).delete()
+			usuario.save()
+			usuario.reload()
+			prova		= usuario.materias.get(_id=materia_id).turmas.get(_id=turma_id).provas.get(_id=prova_id)
 		for key, value in data.items():
 			prova[key]	= value
 		usuario.save()
@@ -64,11 +92,19 @@ def updateProva(response, usuario_id, materia_id, turma_id, prova_id, data):
 		return { "error": "bad_gateway" }
 
 def deleteProva(response, usuario_id, materia_id, turma_id, prova_id):
-	locals	= eval(response.get_header("locals"))
 	try:
-		prova	= Usuario.objects.get(id=usuario_id).materias.get(_id=materia_id).turmas.get(_id=turma_id).provas.get(_id=prova_id)
-		prova.delete()
-		return {}
+		usuario	= Usuario.objects.get(id=usuario_id)
+		usuario.materias.get(_id=materia_id).turmas.get(_id=turma_id).provas.get(_id=prova_id).filter(_id=provaBase_id).delete()
+		usuario.save()
+		return
 	except Exception as e:
 		response.status = HTTP_502
 		return { "error": "bad_gateway" }
+
+def dataProva(data):
+	for item in data["realizacoes"]:
+		item.pop('respostas', None)
+		item.pop('nota', None)
+		item.pop('timestamp', None)
+		item.pop('timeupdate', None)
+	return data
