56d6df40dc813afcda528bc9de84454e4f6d2c87
José Gabriel Gruber <jgabrielgruber@hotmail.com>
2019-10-02 16:17:56 -0300

    Implementado permissão temporária para todos os CORS

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ import	conf
 from	route		import index
 
 api = hug.API(__name__)
+api.http.add_middleware(hug.middleware.CORSMiddleware(api, allow_origins=["*"]))
 
 @hug.extend_api('')
 def index_api():
