04d54b781086802bc3b3f704a73059c4962d444f
José Gabriel Gruber <jgabrielgruber@hotmail.com>
2019-11-07 17:10:48 -0300

    Implementado melhor log dos erros;

--- src/library/errorHandler.py
+++ src/library/errorHandler.py
@@ -1,8 +1,10 @@
 import	hug
 from	falcon		import HTTP_201, HTTP_400, HTTP_403, HTTP_404, HTTP_502
+import traceback
 
 def handleError(response, e):
-	#print("Error: " + e)
+	print("Error: ", e)
+	print(traceback.format_exc())
 	type =  e.__class__.__name__
 	if (type == 'DoesNotExist'):
 		response.status = HTTP_404
